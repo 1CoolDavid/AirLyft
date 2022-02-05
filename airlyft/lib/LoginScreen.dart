0810da4ceae2d80f7e5db8708188a76f6c1a36f7
SeanSun6814 <33432158+SeanSun6814@users.noreply.github.com>
2022-02-05 10:30:22 -0500

    fixed keyboard popup bug

--- airlyft/lib/LoginScreen.dart
+++ airlyft/lib/LoginScreen.dart
@@ -7,104 +7,106 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color(0xFFfbefd9),
-        body: Container(
-            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 45.0),
-            width: 350.0,
-            height: 700.0,
-            decoration: BoxDecoration(
-              color: Color(0xFF5D5B56),
-              borderRadius: BorderRadius.all(const Radius.circular(31.0)),
-            ),
-            child: Column(
-              children: <Widget>[
-                Container(
-                    padding: EdgeInsets.only(top: 70.0),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        fontFamily: "Roboto",
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40.0,
-                        color: Color(0xFFFBEFD9),
-                      ),
-                    )),
-                Container(
-                    padding: EdgeInsets.only(top: 60.0),
-                    child: Text(
-                      "Enter your email                                  ",
-                      style: TextStyle(
-                        fontFamily: "Roboto",
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14.0,
-                        color: Color(0xFFFBEFD9),
-                      ),
-                    )),
-                Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 35.0),
-                    child: TextField(
-                      style: TextStyle(fontSize: 7),
-                      decoration: InputDecoration(
-                        hintText: "",
-                        fillColor: Color(0xFFfbefd9),
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    )),
-                Container(
-                    padding: EdgeInsets.only(top: 25.0),
-                    child: Text(
-                      "Enter your password                             ",
-                      style: TextStyle(
-                        fontFamily: "Roboto",
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14.0,
-                        color: Color(0xFFFBEFD9),
-                      ),
-                    )),
-                Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 35.0),
-                    child: TextField(
-                      style: TextStyle(fontSize: 7),
-                      decoration: InputDecoration(
-                        hintText: "",
-                        fillColor: Color(0xFFfbefd9),
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    )),
-                Container(
-                    padding: EdgeInsets.only(top: 100.0),
-                    child: TextButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Color(0xFF5D5B56)),
-                            shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30.0),
-                                    side:
-                                        BorderSide(color: Color(0xFFFBEFD9))))),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => SignUpScreen()));
-                        },
-                        child: Container(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 55.0),
-                            child: Text('Login',
-                                style: TextStyle(
-                                    color: Color(0xFFFBEFD9),
-                                    fontSize: 20.0)))))
-              ],
-            )));
+        body: SingleChildScrollView(
+            child: Container(
+                margin: EdgeInsets.symmetric(vertical: 35.0, horizontal: 45.0),
+                width: 350.0,
+                height: 610.0,
+                decoration: BoxDecoration(
+                  color: Color(0xFF5D5B56),
+                  borderRadius: BorderRadius.all(const Radius.circular(31.0)),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.only(top: 70.0),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontFamily: "Roboto",
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40.0,
+                            color: Color(0xFFFBEFD9),
+                          ),
+                        )),
+                    Container(
+                        padding: EdgeInsets.only(top: 60.0),
+                        child: Text(
+                          "Enter your email                                  ",
+                          style: TextStyle(
+                            fontFamily: "Roboto",
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+                            color: Color(0xFFFBEFD9),
+                          ),
+                        )),
+                    Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 35.0),
+                        child: TextField(
+                          style: TextStyle(fontSize: 20),
+                          decoration: InputDecoration(
+                            hintText: "",
+                            fillColor: Color(0xFFfbefd9),
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        )),
+                    Container(
+                        padding: EdgeInsets.only(top: 25.0),
+                        child: Text(
+                          "Enter your password                             ",
+                          style: TextStyle(
+                            fontFamily: "Roboto",
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+                            color: Color(0xFFFBEFD9),
+                          ),
+                        )),
+                    Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 35.0),
+                        child: TextField(
+                          style: TextStyle(fontSize: 20),
+                          decoration: InputDecoration(
+                            hintText: "",
+                            fillColor: Color(0xFFfbefd9),
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        )),
+                    Container(
+                        padding: EdgeInsets.only(top: 100.0),
+                        child: TextButton(
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<Color>(
+                                    Color(0xFF5D5B56)),
+                                shape:
+                                    MaterialStateProperty.all<RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30.0),
+                                            side: BorderSide(
+                                                color: Color(0xFFFBEFD9))))),
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => SignUpScreen()));
+                            },
+                            child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 10.0, horizontal: 55.0),
+                                child: Text('Login',
+                                    style: TextStyle(
+                                        color: Color(0xFFFBEFD9),
+                                        fontSize: 20.0)))))
+                  ],
+                ))));
   }
 }
